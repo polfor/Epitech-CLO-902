0da388b553b9b7a9af0fe750348d63e3aefe096f
Paul Forsans <polfor33@gmail.com>
2024-05-30 16:48:49 +0200

    tried to setup a load balancer (failed due to permissions)

--- terraform/main.tf
+++ terraform/main.tf
@@ -25,25 +25,16 @@ resource "azurerm_dev_test_lab" "lab" {
   }
 }
 
-resource "azurerm_dev_test_virtual_network" "network" {
+data "azurerm_dev_test_virtual_network" "network" {
   name                = "t-clo-902-bdx-2"
   lab_name            = azurerm_dev_test_lab.lab.name
   resource_group_name = azurerm_resource_group.resource-group.name
-
-  subnet {
-    use_public_ip_address           = "Allow"
-    use_in_virtual_machine_creation = "Allow"
-  }
-
-  tags = {
-    "city"               = "bdx"
-    "cost:application"   = "socle-student"
-    "cost:business-unit" = "epitech"
-    "cost:environment"   = "prod"
-    "group_number"       = "2"
-    "module"             = "t-clo-902"
-  }
 }
+
+output "lab_subnet_name" {
+  value = data.azurerm_dev_test_virtual_network.network.allowed_subnets[0].lab_subnet_name
+}
+
 
 resource "azurerm_dev_test_linux_virtual_machine" "vm1" {
   name                   = "k8s-node1"
@@ -53,9 +44,10 @@ resource "azurerm_dev_test_linux_virtual_machine" "vm1" {
   size                   = "Standard_A4_v2"
   username               = "epitechusr"
   ssh_key                = file("~/.ssh/id_rsa.pub")
-  lab_virtual_network_id = azurerm_dev_test_virtual_network.network.id
-  lab_subnet_name        = azurerm_dev_test_virtual_network.network.subnet[0].name
+  lab_virtual_network_id = data.azurerm_dev_test_virtual_network.network.id
+  lab_subnet_name        = data.azurerm_dev_test_virtual_network.network.allowed_subnets[0].lab_subnet_name
   storage_type           = "Standard"
+  allow_claim            = false
   notes                  = "no"
 
   gallery_image_reference {
@@ -77,9 +69,10 @@ resource "azurerm_dev_test_linux_virtual_machine" "vm2" {
   size                   = "Standard_A4_v2"
   username               = "epitechusr"
   ssh_key                = file("~/.ssh/id_rsa.pub")
-  lab_virtual_network_id = azurerm_dev_test_virtual_network.network.id
-  lab_subnet_name        = azurerm_dev_test_virtual_network.network.subnet[0].name
+  lab_virtual_network_id = data.azurerm_dev_test_virtual_network.network.id
+  lab_subnet_name        = data.azurerm_dev_test_virtual_network.network.allowed_subnets[0].lab_subnet_name
   storage_type           = "Standard"
+  allow_claim            = false
   notes                  = "no"
 
   gallery_image_reference {
@@ -102,9 +95,10 @@ resource "azurerm_dev_test_linux_virtual_machine" "vm4" {
   size                   = "Standard_A4_v2"
   username               = "epitechusr"
   ssh_key                = file("~/.ssh/id_rsa.pub")
-  lab_virtual_network_id = azurerm_dev_test_virtual_network.network.id
-  lab_subnet_name        = azurerm_dev_test_virtual_network.network.subnet[0].name
+  lab_virtual_network_id = data.azurerm_dev_test_virtual_network.network.id
+  lab_subnet_name        = data.azurerm_dev_test_virtual_network.network.allowed_subnets[0].lab_subnet_name
   storage_type           = "Standard"
+  allow_claim            = false
   notes                  = "no"
 
   gallery_image_reference {
@@ -116,5 +110,24 @@ resource "azurerm_dev_test_linux_virtual_machine" "vm4" {
 
   tags = {
     "k8s-type" = "control-plane"
+  }
+}
+
+resource "azurerm_public_ip" "load-balancer-public-ip" {
+  name                = "LoadBalancerPublicIP"
+  location            = azurerm_resource_group.resource-group.location
+  resource_group_name = azurerm_resource_group.resource-group.name
+  allocation_method   = "Static"
+}
+
+resource "azurerm_lb" "load-balancer" {
+  name                = "LoadBalancer"
+  location            = azurerm_resource_group.resource-group.location
+  resource_group_name = azurerm_resource_group.resource-group.name
+
+  frontend_ip_configuration {
+    name                 = "PublicIPAddress"
+    public_ip_address_id = azurerm_public_ip.load-balancer-public-ip.id
+    subnet_id            = data.azurerm_dev_test_virtual_network.network.allowed_subnets[0].resource_id
   }
 }
