ae7872cf030bfdf1689649341b46b169c7c9ae9d
Paul Forsans <polfor33@gmail.com>
2024-05-30 11:11:33 +0200

    edited terraform

--- terraform/main.tf
+++ terraform/main.tf
@@ -46,7 +46,7 @@ resource "azurerm_dev_test_virtual_network" "network" {
 }
 
 resource "azurerm_dev_test_linux_virtual_machine" "vm1" {
-  name                   = "k8s-pod1"
+  name                   = "k8s-node1"
   lab_name               = azurerm_dev_test_lab.lab.name
   resource_group_name    = azurerm_resource_group.resource-group.name
   location               = azurerm_resource_group.resource-group.location
@@ -70,7 +70,7 @@ resource "azurerm_dev_test_linux_virtual_machine" "vm1" {
   }
 }
 resource "azurerm_dev_test_linux_virtual_machine" "vm2" {
-  name                   = "k8s-pod2"
+  name                   = "k8s-node2"
   lab_name               = azurerm_dev_test_lab.lab.name
   resource_group_name    = azurerm_resource_group.resource-group.name
   location               = azurerm_resource_group.resource-group.location
@@ -93,30 +93,7 @@ resource "azurerm_dev_test_linux_virtual_machine" "vm2" {
     "k8s-type" = "worker"
   }
 }
-resource "azurerm_dev_test_linux_virtual_machine" "vm3" {
-  name                   = "k8s-pod3"
-  lab_name               = azurerm_dev_test_lab.lab.name
-  resource_group_name    = azurerm_resource_group.resource-group.name
-  location               = azurerm_resource_group.resource-group.location
-  size                   = "Standard_A4_v2"
-  username               = "epitechusr"
-  ssh_key                = file("~/.ssh/id_rsa.pub")
-  lab_virtual_network_id = azurerm_dev_test_virtual_network.network.id
-  lab_subnet_name        = azurerm_dev_test_virtual_network.network.subnet[0].name
-  storage_type           = "Standard"
-  notes                  = "no"
 
-  gallery_image_reference {
-    offer     = "UbuntuServer"
-    publisher = "Canonical"
-    sku       = "18.04-LTS"
-    version   = "latest"
-  }
-
-  tags = {
-    "k8s-type" = "worker"
-  }
-}
 resource "azurerm_dev_test_linux_virtual_machine" "vm4" {
   name                   = "k8s-control-plane"
   lab_name               = azurerm_dev_test_lab.lab.name
